b3b6320506a7fc7190280d0b591eb9a1bdb00d11
Dhrumil Mistry <56185972+dmdhrumilmistry@users.noreply.github.com>
2023-03-29 09:55:16 +0530

    add prog name

--- shift_left_secure/__main__.py
+++ shift_left_secure/__main__.py
@@ -2,7 +2,7 @@ from os.path import isdir
 from argparse import ArgumentParser
 
 if __name__ == '__main__':
-    parser = ArgumentParser()
-    parser.add_argument('-d', '--directory', help='directory of git project', dest='directory', type=str, default='.')
+    parser = ArgumentParser(prog='shift_left_secure')
+    parser.add_argument('-d', '--directory', help='directory of git project', dest='directory', type=str, required=True)
 
     args = parser.parse_args()
